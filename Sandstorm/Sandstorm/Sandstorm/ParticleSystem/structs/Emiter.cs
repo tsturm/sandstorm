31a999d2c1b52fec9b9150477a4db73eb3c3c703
sascha.arthur@gmail.com <sascha.arthur@gmail.com>
2013-05-08 14:47:52 +0200

    emiteranpassung

--- Sandstorm/Sandstorm/Sandstorm/ParticleSystem/structs/Emiter.cs
+++ Sandstorm/Sandstorm/Sandstorm/ParticleSystem/structs/Emiter.cs
@@ -6,6 +6,9 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Sandstorm.ParticleSystem.structs;
 
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Sandstorm.ParticleSystem
 {
     class Emiter
@@ -15,7 +18,7 @@ namespace Sandstorm.ParticleSystem
         SharedList _sharedlist;
         private Random _rand = new Random();
         
-        private static float MIN_RAND = 0.01f;
+        private static float MIN_RAND = -0.5f;
         private static float MAX_RAND = 0.5f;
         public Emiter(Vector3 pos, Vector3 force, SharedList sharedlist)
         {
@@ -26,20 +29,22 @@ namespace Sandstorm.ParticleSystem
 
         private float getRandomFloat(float min, float max)
         {
-            return (float)(min + (_rand.NextDouble() * (max - min)));
+            return (float)((_rand.NextDouble() * (Math.Abs(max) + Math.Abs(min)) - Math.Abs(min)));
         }
+
         private Vector3 getSmallRandomForce()
         {
             return new Vector3(this.getRandomFloat(MIN_RAND, MAX_RAND), this.getRandomFloat(MIN_RAND, MAX_RAND), this.getRandomFloat(MIN_RAND, MAX_RAND));
         }
         public void emit()
         {
-            Particle p = new Particle(this._pos, this._force);
+            Parallel.For(0, 1, i =>
+            {
 
-            p.applyExternalForce(this.getSmallRandomForce());
-
-            for(int i=0; i < 100;i++)
+                Particle p = new Particle(this._pos, this._force);
+                p.applyExternalForce(this.getSmallRandomForce());
                 this._sharedlist.addParticle(p);
+            });
         }
     }
 }
